f147be7a0974a3e76fb9b16e23a0792f4148ff9b
kaye wu <apiswswsw@gmail.com>
2014-09-09 22:09:38 -0700

    allow stellarresult to show error response.

--- stellarj/src/me/xbt/stellarj/stellard/rpc/result/StellarResult.java
+++ stellarj/src/me/xbt/stellarj/stellard/rpc/result/StellarResult.java
@@ -4,6 +4,9 @@ import flexjson.JSONSerializer;
 
 /**
  * 
+ * the result could also be an error response.  
+ * eg. {   "result" : {      "error" : "invalidParams",      "error_code" : 27,      "error_message" : "Missing field 'taker_pays'.",      "request" : {         "command" : "book_offers"      },      "status" : "error"   }}
+ * 
  * @author kaye wu
  *
  */
@@ -11,7 +14,12 @@ public class StellarResult {
 	/** to show a value is uninitiated */
 	public static final int UNINITIATED = -2;
 
-	private String status;
+	private String status = null;
+	
+	// for error response.  these will only be populated when status == "error"
+	private String error = null;
+	private Long error_code = null;
+	private String error_message = null;
 
 	public String getStatus() {
 		return status;
@@ -21,6 +29,31 @@ public class StellarResult {
 		this.status = status;
 	}
 	
+	
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public Long getError_code() {
+		return error_code;
+	}
+
+	public void setError_code(Long error_code) {
+		this.error_code = error_code;
+	}
+
+	public String getError_message() {
+		return error_message;
+	}
+
+	public void setError_message(String error_message) {
+		this.error_message = error_message;
+	}
+
 	
 	public String toString() {
 		return new JSONSerializer().prettyPrint(true).deepSerialize(this);
